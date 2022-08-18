6ca7775654f1bc26a61a22a442998303609b3ff8
Alipa-Mizamova <110773073+Alipa-Mizamova@users.noreply.github.com>
2022-08-18 18:10:57 +0600

    Add files via upload

---
+++ files/file_executor/__init__.py
@@ -0,0 +1,12 @@
+import pathlib
+class Files:
+    def __init__(self, ):
+        self.creat = creat
+        self.write = write
+        self.delete = delete
+class Creat:
+    pass
+class Write:
+    pass
+class Delete:
+    pass
